c1a81a34cda99718408b8dea8bd4ec2f67fc95f3
Bodo Tasche <bodo@wannawork.de>
2018-03-10 17:18:34 +0100

    finished working rss feed

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import KoaViews from 'koa-views';
 import KoaStatic from 'koa-static';
 import path from 'path';
 
+import FetchImdb from './fetch_imdb.mjs';
+
 const app = new Koa();
 const router = new KoaRouter();
 
@@ -18,6 +20,13 @@ app.use(KoaStatic('./assets'));
 
 router.get('/', async (ctx) => {
   await ctx.render('index');
+});
+
+router.get('/rss/:user_id', async (ctx) => {
+  const rss = await FetchImdb(ctx.params.user_id);
+
+  ctx.type = 'application/rss+xml';
+  ctx.body = rss;
 });
 
 app
